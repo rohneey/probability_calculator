664b7ce1c78dda8e5e7ad9888c87084fb79ae974
Veronika <veronika.fadeeva@aflex.ru>
2022-12-27 00:39:43 +0100

    Add test and delete test... just to test

--- test_module.py
+++ test_module.py
@@ -25,12 +25,12 @@ class UnitTests(unittest.TestCase):
         actual = probability
         expected = 0.272
         self.assertAlmostEqual(actual, expected, delta = 0.01, msg = 'Expected experiment method to return a different probability.')
+    
         hat = prob_calculator.Hat(yellow=5,red=1,green=3,blue=9,test=1)
         probability = prob_calculator.experiment(hat=hat, expected_balls={"yellow":2,"blue":3,"test":1}, num_balls_drawn=20, num_experiments=100)
         actual = probability
         expected = 1.0
         self.assertAlmostEqual(actual, expected, delta = 0.01, msg = 'Expected experiment method to return a different probability.')
-
 
 if __name__ == "__main__":
     unittest.main()
